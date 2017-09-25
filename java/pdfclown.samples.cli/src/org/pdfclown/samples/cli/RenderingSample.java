568872abf232b3e7b8a754eafd608b3220a5d288
Rohit Kumar <rohitvishal7@gmail.com>
2017-09-25 15:10:43 +0530

    Update RenderingSample
    The change ensures rendering of png format images. It uses pdfview.

--- java/pdfclown.samples.cli/src/org/pdfclown/samples/cli/RenderingSample.java
+++ java/pdfclown.samples.cli/src/org/pdfclown/samples/cli/RenderingSample.java
@@ -1,16 +1,17 @@
 package org.pdfclown.samples.cli;
 
-import java.awt.geom.Dimension2D;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import javax.imageio.ImageIO;
+import com.sun.pdfview.PDFFile;
+import com.sun.pdfview.PDFPage;
 
-import org.pdfclown.documents.Document;
-import org.pdfclown.documents.Page;
-import org.pdfclown.documents.Pages;
-import org.pdfclown.files.File;
-import org.pdfclown.tools.Renderer;
 
 /**
   This sample demonstrates <b>how to render a PDF page as a raster image</b>.
@@ -28,43 +29,62 @@ public class RenderingSample
   public void run(
     )
   {
-    File file = null;
+    java.io.File file = null;
     try
     {
       // 1. Opening the PDF file...
       {
         String filePath = promptFileChoice("Please select a PDF file");
         try
-        {file = new File(filePath);}
+        {
+        file = new java.io.File(filePath);
+        
+        }
         catch(Exception e)
         {throw new RuntimeException(filePath + " file access error.",e);}
       }
-      Document document = file.getDocument();
-      Pages pages = document.getPages();
+      
+      try
+      {
+      java.io.RandomAccessFile raf = new java.io.RandomAccessFile(file, "r");
+  	FileChannel channel = raf.getChannel();
+  	ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+  	PDFFile pdf = new PDFFile(buf);
+      
 
       // 2. Page rasterization.
-      int pageIndex = promptPageChoice("Select the page to render", pages.size());
-      Page page = pages.get(pageIndex);
-      Dimension2D imageSize = page.getSize();
-      Renderer renderer = new Renderer();
-      BufferedImage image = renderer.render(page, imageSize);
+  	
+      int pageIndex = promptPageChoice("Select the page to render", pdf.getNumPages());
+      
+      PDFPage myPage = pdf.getPage(pageIndex);
+      Rectangle rect = new Rectangle(0, 0, (int) myPage.getBBox().getWidth(),
+              (int) myPage.getBBox().getHeight());
+BufferedImage bufferedImage = new BufferedImage(rect.width, rect.height,
+               BufferedImage.TYPE_INT_RGB);
+
+Image image = myPage.getImage(rect.width, rect.height,    // width & height
+         rect,                       // clip rect
+         null,                       // null for the ImageObserver
+         true,                       // fill background with white
+         true                        // block until drawing is done
+);
+Graphics2D bufImageGraphics = bufferedImage.createGraphics();
+bufImageGraphics.drawImage(image, 0, 0, null);
+
+
+raf.close();
+           
 
       // 3. Save the page image!
-      try
-      {ImageIO.write(image,"jpg",new java.io.File(getOutputPath("ContentRenderingSample.jpg")));}
+      ImageIO.write(bufferedImage,"jpg",new java.io.File(getOutputPath("ContentRenderingSample"+pageIndex+".jpg")));}
+      catch(FileNotFoundException f)
+      {
+    	  f.printStackTrace();
+      }
       catch(IOException e)
       {e.printStackTrace();}
+      
     }
-    finally
-    {
-      // 4. Closing the PDF file...
-      if(file != null)
-      {
-        try
-        {file.close();}
-        catch(IOException e)
-        {/* NOOP */}
-      }
-    }
+   
   }
 }
